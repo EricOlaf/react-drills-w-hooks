724f1602476bd4361f05cd9e7c631c0c64fc3895
Eric Olaveson <olavesoneric@gmail.com>
2019-11-21 20:49:16 -0600

    new manipulation for strings

--- react-max-2/src/App.js
+++ react-max-2/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ValidationComponent from './ValidationComponent/ValidationComponent'
+
+class App extends Component{
+  state={
+    userInput:""
+  }
+
+  changeHandler = (event) => {
+    const newInput = event.target.value;
+    this.setState({userInput: newInput})
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>My cool new site</h1>
+        <input value={this.state.userInput} onChange={this.changeHandler}/>
+        <ValidationComponent stringLength={this.state.userInput.length}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ react-max-2/src/ValidationComponent/ValidationComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ValidationComponent = props =>{
+    return(
+        <div>
+            <p>{props.stringLength}</p>
+        </div>
+    )
+}
+export default ValidationComponent

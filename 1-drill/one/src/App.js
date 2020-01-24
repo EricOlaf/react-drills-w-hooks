62980fbb2f69d8f1e7bbbbc580f3d39d0f7b4455
Eric Olaveson <olavesoneric@gmail.com>
2020-01-23 18:36:38 -0600

    new react drill 1 with hooks

--- 1-drill/one/src/App.js
+++ 1-drill/one/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state={
+    userInput:""
+  }
+
+  inputHandler = e => {
+    this.setState({userInput: e.target.value})
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <section>
+          <p>{this.state.userInput}</p>
+          <input placeholder="enter some value" value={this.state.userInput} onChange={(e)=>{this.inputHandler(e)}}/>
+        </section>
+      </div>
+    );
+  }  
 }
 
 export default App;
